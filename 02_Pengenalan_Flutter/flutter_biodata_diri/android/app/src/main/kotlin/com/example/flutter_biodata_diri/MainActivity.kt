2339433c3a41838a3b02d790da030c2152a177bb
Fauzan Wahyu Mubarak <114811887+FauzanWahyuM@users.noreply.github.com>
2024-09-29 11:44:17 +0700

    Update Praktikum 2

---
+++ 02_Pengenalan_Flutter/flutter_biodata_diri/android/app/src/main/kotlin/com/example/flutter_biodata_diri/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_biodata_diri
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- 02_Pengenalan_Flutter/flutter_coba/android/app/src/main/kotlin/com/example/flutter_coba/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_coba
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
