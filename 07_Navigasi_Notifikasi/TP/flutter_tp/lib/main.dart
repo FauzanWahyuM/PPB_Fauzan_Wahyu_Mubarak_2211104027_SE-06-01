06f8d14f4a6edf4c08d29d83b1bcf2020c5de43d
Fauzan Wahyu Mubarak <114811887+FauzanWahyuM@users.noreply.github.com>
2024-10-30 18:15:49 +0700

    Updated Pratikum 7

---
+++ 07_Navigasi_Notifikasi/TP/flutter_tp/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Halaman Pertama"),
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+          },
+          child: Text("Ke Halaman Kedua"),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.amber,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Halaman Kedua"),
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+          child: Text(
+        "Ini adalah halaman kedua",
+        style: TextStyle(fontSize: 24),
+      )),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirstPage(),
+    );
+  }
+}
